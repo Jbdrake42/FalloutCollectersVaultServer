1541c2ef2789292bf0a150e652657cd832cc7642
James Drake <jbdrake42@gmail.com>
2021-11-16 12:01:24 -0500

    marco please help!!

--- db.js
+++ db.js
@@ -2,6 +2,13 @@ const { Sequelize } = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+        require: true,
+        rejectUnauthorized: false, // very important
+      }
+  }
+
 })
 
 module.exports = db;
